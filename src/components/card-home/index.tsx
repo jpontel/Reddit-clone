adf24a5c0d0a014235bd586fe2aff80c2edb73f5
jpontel <jpontel2002@gmail.com>
2024-04-18 00:10:40 -0300

    implementing card and data formatting...DONE

--- src/components/card-home/index.tsx
+++ src/components/card-home/index.tsx
@@ -14,7 +14,7 @@ interface CardHomeProps {
 
 export default function CardHome({name, time, description, tags, likes, comments, marks}: CardHomeProps) {
     return (
-        <div className={'w-[40rem] min-h-min bg-white shadow-lg border-2 rounded-lg'}
+        <div className={'w-[40rem] mt-6 min-h-min bg-white shadow-lg border-2 rounded-lg'}
              style={{backdropFilter: 'blur(40.774227142333984px)'}}>
             <div className={'items-center justify-center inline-flex ml-4 mt-4'}>
                 <div>
@@ -22,34 +22,42 @@ export default function CardHome({name, time, description, tags, likes, comments
                 </div>
                 <div className={'flex-col'}>
                     <div className={'ml-4 items-center justify-center font-bold'}>
-                        José Rodrigo Pontel
+                        {name}
                     </div>
                     <div className={'ml-4 items-center justify-center font-regular text-slate-400 text-[10px]'}>
-                        10 min atrás
+                        {time}
                     </div>
                 </div>
             </div>
             <div className={'mt-2 mx-4'}>
                 <div className={'min-h-min'}>
-                    Description
+                    {description}
                 </div>
                 <div className={'mt-2 text-slate-400 text-[12px]'}>
-                    #Gaming #PC #Setup #Gamer #Battlestation
+                    {tags.map((tag, index) => {
+                        return (
+                            <span key={index} className={'mr-2'}>
+                                #{tag}
+                            </span>
+                        );
+                    })}
                 </div>
             </div>
             <div className={'w-full inline-flex mt-4 mb-4 justify-between'}>
                 <div className={'inline-flex ml-4 gap-3'}>
                     <div className={'inline-flex'}>
-                        <img src={likeIcon} className={'w-4 mr-1'} alt={'Like Icon'}/>
-                        <div className={'font-medium'}>3</div>
+                        <button>
+                            <img src={likeIcon} className={'w-4 mr-1 hover:bg-red-500'} alt={'Like Icon'}/>
+                        </button>
+                        <div className={'font-medium'}>{likes}</div>
                     </div>
                     <div className={'inline-flex'}>
                         <img src={commentIcon} className={'w-4 mr-1'} alt={'Comment Icon'}/>
-                        <div className={'font-medium'}>3</div>
+                        <div className={'font-medium'}>{comments}</div>
                     </div>
                     <div className={'inline-flex'}>
                         <img src={markedIcon} className={'w-3 mr-1'} alt={'Marked Icon'}/>
-                        <div className={'font-medium'}>3</div>
+                        <div className={'font-medium'}>{marks}</div>
                     </div>
                 </div>
                 <div className={'justify-end items-end inline-flex gap-2 mr-4'}>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,67 @@
 import CardHome from "../../components/card-home";
+import {useEffect, useState} from "react";
+import moment from "moment";
+
+interface HomeProps {
+    name: string;
+    time: string;
+    description: string;
+    tags: string[];
+    likes: number;
+    comments: number;
+    marks: number;
+}
 
 export default function Home() {
-  return (
-    <div className={'w-full h-full inline-flex items-center justify-center mt-12'}>
-        <div className={'items-center justify-center'}>
-            <CardHome />
+    const [data, setData] = useState<HomeProps[]>([]);
+
+    function formatTime(time:string){
+        const timeMoment = moment(time, 'YYYYMMDD HH:mm:ss')
+        return timeMoment.fromNow();
+    }
+
+    useEffect(() => {
+            setData([
+                {
+                    name: 'José Rodrigo Pontel',
+                    time: formatTime('20240415 12:00:00'),
+                    description: 'EU Amo ver animes!! olha que coisa legal!! Vejam Madoka Magica, quem concorda dá like :)!!',
+                    tags: ['Anime', 'Madoka Magica', 'Otaku', 'Weeb'],
+                    likes: 23,
+                    comments: 12,
+                    marks: 2
+                },
+                {
+                    name: 'Andrei Amorim',
+                    time: formatTime('20240415 18:30:00'),
+                    description: 'EU Amo ver séries!! American Horror Story é a minha favotira!!',
+                    tags: ['Horror', 'Series', 'Netflix', 'Twitter'],
+                    likes: 590,
+                    comments: 120,
+                    marks: 45
+                },
+            ])
+        },
+        []
+    );
+
+    return (
+        <div className={'w-full h-full inline-flex items-center justify-center mt-12'}>
+            <div className={'items-center justify-center'}>
+                {data.map((item, index) => {
+                    return (
+                        <CardHome key={index}
+                                  name={item.name}
+                                  time={item.time.toString()}
+                                  description={item.description}
+                                  tags={item.tags}
+                                  likes={item.likes}
+                                  comments={item.comments}
+                                  marks={item.marks}
+                        />
+                    );
+                })}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
